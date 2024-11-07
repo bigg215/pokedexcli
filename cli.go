37bf531d232901af7a2bee7aef7ee5489de60f84
big215 <glenn.j.gilroy@gmail.com>
2024-11-06 22:06:48 -0500

    internal json commands

---
+++ cli.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+
+		if exists {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+	}
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*config) error
+}
+
+type config struct {
+	Next     *string
+	Previous *string
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Display a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"map": {
+			name:        "map",
+			description: "List next 20 locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "List previous 20 locations",
+			callback:    commandMapb,
+		},
+	}
+}
---
+++ command_map.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/bigg215/pokedexcli/internal/papi"
+)
+
+func commandMapf(cfg *config) error {
+	resp, err := papi.GetLocations(cfg.Next)
+	if err != nil {
+		return err
+	}
+	cfg.Next = resp.Next
+	cfg.Previous = resp.Previous
+	for _, loc := range resp.Results {
+		fmt.Println(loc.Name)
+	}
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.Previous == nil {
+		return errors.New("you're on the first page")
+	}
+	resp, err := papi.GetLocations(cfg.Previous)
+	if err != nil {
+		return err
+	}
+	cfg.Next = resp.Next
+	cfg.Previous = resp.Previous
+	for _, loc := range resp.Results {
+		fmt.Println(loc.Name)
+	}
+	return nil
+}
